6d7f782bdfcc3fa3866251232f401e199ed81fa4
Oleg <bezrukov-oa@nm.ru>
2021-01-25 02:54:11 +0300

    lesson1

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainapp/index.html')
-
+# коментарий для того чтобы новая ветвь отличалась от master
 
 def products(request):
     return render(request, 'mainapp/products.html')
