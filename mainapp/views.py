8a5da202a1844fec2c18618beb1b6d260c63eeb8
Oleg <bezrukov-oa@nm.ru>
2021-02-17 01:16:41 +0300

    add goods, prepare for working

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,9 +7,10 @@ def index(request):
     return render(request, 'mainapp/index.html')
 # коментарий для того чтобы новая ветвь отличалась от master
 
-def products(request, id=None):
+def products(request, category_id=None):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
     context = {
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
+            'categories': ProductCategory.objects.all(),
+            'products': products
+        }
     return render(request, 'mainapp/products.html', context)
