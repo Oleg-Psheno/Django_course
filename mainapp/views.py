0e028769c6a4ef6ca7cb1ae1f92ebf0f08473e25
Oleg <bezrukov-oa@nm.ru>
2021-02-17 01:42:56 +0300

    add ordering for goods in products

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,9 +18,9 @@ def index(request):
 
 def products(request, category_id=None, page=1):
     if category_id:
-        products = Product.objects.filter(category_id=category_id)
+        products = Product.objects.filter(category_id=category_id).order_by('price')
     else:
-        products = Product.objects.all()
+        products = Product.objects.all().order_by('price')
     per_page = 3
     paginator = Paginator(products, per_page)
     products_paginator = paginator.page(page)
@@ -29,11 +29,6 @@ def products(request, category_id=None, page=1):
         'products': products_paginator
     }
     return render(request, 'mainapp/products.html', context)
-    # try:
-    #     products_paginator = paginator.page(page)
-    # except PageNotAnInteger:
-    #     products_paginator = paginator.page(1)
-    # except EmptyPage:
-    #     products_paginator = paginator.page(paginator.num_pages)
+
 
 
