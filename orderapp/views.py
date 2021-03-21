be6a4389fdcb9c4bacf5ea8db6d9c132509ce0b8
Oleg <bezrukov-oa@nm.ru>
2021-03-21 19:56:31 +0300

    1

--- orderapp/views.py
+++ orderapp/views.py
@@ -19,21 +19,26 @@ class OrderItemsCreate(CreateView):
     model = Order
     fields = []
     success_url = reverse_lazy('orderapp:order_list')
-    queryset = Basket.objects.all()
-
-
 
     def get_context_data(self, **kwargs):
         data = super(OrderItemsCreate, self).get_context_data(**kwargs)
-        OrderFormSet = inlineformset_factory(Order,OrderItem,form=OrderItemForm, extra=1)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
         if self.request.POST:
             formset = OrderFormSet(self.request.POST)
         else:
-            basket_items = Basket.objects.filter(user = self.request.user)
-            # basket_items = Basket.get_items(self.request.user)
+            # basket_items = Basket.objects.filter(user = self.request.user)
+            basket_items = Basket.get_items(self.request.user)
             if len(basket_items):
-                OrderFormSet = inlineformset_factory(Order,OrderItem, form=OrderItemForm, extra=len(basket_items))
+                OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
                 formset = OrderFormSet()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+                basket_items.delete()
+            else:
+                formset = OrderFormSet()
+
         data['orderitems'] = formset
         return data
 
